fb001ca8e98fe4e2f6f9d864556165a5346277f8
Ayasha Khan <144647351+KhanAyasha@users.noreply.github.com>
2023-11-27 12:39:09 -0500

    Delete Assignment9 directory

--- Assignment9/backend/app/router/router.js
+++
@@ -1,9 +0,0 @@
-// router.js
-import express from 'express';
-import userRouter from './userRouter';
-
-const router = express.Router();
-
-router.use('/user', userRouter);
-
-export default router;
